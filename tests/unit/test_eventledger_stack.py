2e633748d9144d901a8bfaeb01ecb3deacbea3ff
gl1z <bb.markk@gmail.com>
2026-04-22 17:07:50 +0100

    replace boilerplate stack test with actual assertions

--- tests/unit/test_eventledger_stack.py
+++ tests/unit/test_eventledger_stack.py
@@ -1,15 +1,29 @@
 import aws_cdk as core
 import aws_cdk.assertions as assertions
-
 from eventledger.eventledger_stack import EventledgerStack
 
-# example tests. To run these tests, uncomment this file along with the example
-# resource in eventledger/eventledger_stack.py
-def test_sqs_queue_created():
+def test_stack_has_lambda():
     app = core.App()
     stack = EventledgerStack(app, "eventledger")
     template = assertions.Template.from_stack(stack)
+    template.has_resource_properties("AWS::Lambda::Function", {
+        "Handler": "ingest.handler",
+        "Runtime": "python3.12",
+    })
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+def test_stack_has_dynamodb_table():
+    app = core.App()
+    stack = EventledgerStack(app, "eventledger")
+    template = assertions.Template.from_stack(stack)
+    template.has_resource_properties("AWS::DynamoDB::Table", {
+        "BillingMode": "PAY_PER_REQUEST",
+    })
+
+def test_stack_has_dashboard():
+    app = core.App()
+    stack = EventledgerStack(app, "eventledger")
+    template = assertions.Template.from_stack(stack)
+    template.has_resource_properties("AWS::CloudWatch::Dashboard", {
+        "DashboardName": "eventledger",
+    })
+    
